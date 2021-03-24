bdb6e75fc107dd205ae00f6ed7c6a222211f160b
oukouri <939594458@qq.com>
2021-03-24 22:22:55 +0800

    上传作业

--- py_oop/py_record_oop/start.py
+++ py_oop/py_record_oop/start.py
@@ -2,7 +2,7 @@
 # @Author  : whl
 # @File    : start.py
 from py_oop.py_record_oop.send import send_money
-from py_opp.py_record_opp.select import select_money
+from py_oop.py_record_oop.select import select_money
 
 if __name__ == '__main__':
     send_money(select_money(1000))
